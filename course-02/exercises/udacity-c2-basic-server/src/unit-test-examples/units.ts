0d02c5fc1b5ba925b650ec34594b1843d26280e6
mk-hill <40850298+mk-hill@users.noreply.github.com>
2020-04-18 21:22:41 -0500

    Create additional endpoints and tests

--- course-02/exercises/udacity-c2-basic-server/src/unit-test-examples/units.ts
+++ course-02/exercises/udacity-c2-basic-server/src/unit-test-examples/units.ts
@@ -1,17 +1,25 @@
 // Super Simple Unit Functions
 
 export const add = (a: number, b: number) => {
-    return a + b;
-    }
+  return a + b;
+};
 
 export const divide = (a: number, b: number) => {
-    if(b === 0) { throw new Error('div by 0') }
+  if (b === 0) {
+    throw new Error('div by 0');
+  }
 
-    return a / b;
-    }
+  return a / b;
+};
 
 // @TODO try creating a method "concat" to concatenate two strings
-// it should take two string paramaters.
+// it should take two string parameters.
 // it should return one string combining the two strings.
 // it should throw an error if either of the strings are empty.
 // ensure your function is exported.
+export const concat = (a: string, b: string) => {
+  if (!a || !b) {
+    throw new Error('missing string(s)');
+  }
+  return a + b;
+};
